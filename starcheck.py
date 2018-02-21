40b3a9078dcac05e24d8edabb47b9663a886cc7f
klar <arvet.fuchs@gmail.com>
2018-02-21 14:07:54 +0100

    added update existing repos

--- starcheck.py
+++ starcheck.py
@@ -9,25 +9,27 @@ import subprocess
 import traceback
 import os
 
-username = "myusername"
-password = "mypassword"
+directory = "/var/data/scripts/starchecker/repos/"
+username = ""
+password = ""
 
 try:
 	g = Github(username, password)
 
 	starred_repos = g.get_user().get_starred()
-	
 	for repo in starred_repos:
 		user_reponame = repo._full_name.value
 		if not os.path.exists(repo._name.value):
 			print("doing a clone of %(user_reponame)s" % locals())
 			repoclone = "https://github.com/%(user_reponame)s" % locals()
-			subprocess.call(['git', 'clone', repoclone, '/var/data/scripts/starchecker/repos/' + repo._name.value])
+			subprocess.call(['git', 'clone', repoclone, directory + repo._name.value])
 
 			print("done: %(user_reponame)s" % locals())
 
 	print("updating repos...")
-	repo_update = subprocess.call("ls | xargs -I{} git -C {} pull", shell=True)
-
+	for repo in os.listdir(directory):
+		print repo + ":"
+		os.chdir(directory + repo)
+		subprocess.call(['git', 'pull'])
 except:
 	print(str(traceback.format_exc()))
